31804d1cd02585127573ee9747eb636e3794517b
Qweci Kuranchie <nuhurichard47@gmail.com>
2026-04-14 03:47:11 +0000

    feat: implement Footer component with newsletter subscription and add supporting UI lists and server action

--- development/apps/web/src/components/ui/ClientCategoryList.tsx
+++ development/apps/web/src/components/ui/ClientCategoryList.tsx
@@ -3,7 +3,6 @@
 import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { Breadcrumbs } from "./Breadcrumbs";
 
 interface Category {
   name: string;
